84190edc9a6be65cb0be442d2dad93e8c5c5b0e4
hughhogue36 <hvh18236@gmail.com>
2023-08-16 18:02:40 -0500

    Corrected Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -10,15 +10,15 @@ namespace SyntaxAndSyntaxSugarExercise
             //string response;
             //if (answer < 9)
             //{
-            //    response = answer + " is less than nine";
+            //    response = answer + "is less than nine";
             //}
             //else
             //{
-            //    response = answer + " is greater than or equal to nine";
+            //    response = answer + "is greater than or equal to nine";
             //}
 
             var answer = 4;
-            var response = (answer < 9) ? $"{answer}" is less than nine : $"{answer} is greater than or equal to nine";
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
 
             Console.WriteLine(response);
         }
